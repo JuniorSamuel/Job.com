7cfde686a4e2d9615560df752092d26e43cf59ef
Junior Samuel De Los Santos Velazquez <juniorvelazquezmt19@gmail.com>
2021-07-02 20:25:28 -0400

    manual de usuarios

--- API/DbContexts/APIDbContext.cs
+++ API/DbContexts/APIDbContext.cs
@@ -26,15 +26,6 @@ namespace API.DbContexts
         public virtual DbSet<Usuario> Usuarios { get; set; }
         public virtual DbSet<Vacante> Vacantes { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:jobbddbserver.database.windows.net,1433;Initial Catalog=JobBD;Persist Security Info=False;User ID=Junior;Password=ASanto 2001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
